f999f4b893e458c2355d028ed6da95e35c282a27
drullan <drullan.dev@gmail.com>
2021-02-14 01:52:26 +0100

    Avanzando

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import * as Const from './services/constants'
 
 import React, { useEffect, useState } from 'react'
-import { IonReactRouter } from '@ionic/react-router'
+import { IonReactRouter,  } from '@ionic/react-router'
 
-import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react'
-import { square, triangle, images } from 'ionicons/icons'
+import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar,   useIonViewDidEnter,
+  useIonViewDidLeave,
+  useIonViewWillEnter,
+  useIonViewWillLeave, IonTabButton, IonTabs } from '@ionic/react'
+import { triangle } from 'ionicons/icons'
 import { Redirect, Route } from 'react-router-dom'
-import axios from 'axios'
+//import axios from 'axios'
 
 /* CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css'
@@ -31,84 +34,134 @@ import Training     from './pages/menu/Training'
 import ExploreEquip from './pages/menu/ExploreEquip'
 import Navigate     from './pages/menu/Navigate'
 import Assistance   from './pages/menu/Assistance'
-import Routes       from './pages/menu/Routes'
+import LiveMenu     from './pages/menu/LiveMenu'
+/*import Routes       from './pages/menu/Routes'
 import LiveMap      from './pages/menu/LiveMap'
 import Route1        from './pages/menu/Route1'
-import BoatTypes        from './pages/menu/BoatTypes'
+import BoatTypes        from './pages/menu/BoatTypes'*/
+//import RedirectToLogin from './components/RedirectToLogin';
+
+import { loadConfData } from './data/sessions/sessions.actions';
+import { setIsLoggedIn, setUsername, loadUserData } from './data/user/user.actions';
+
+/*const routes = {
+  appPages: [
+    { name: 'Home',             path: '/Home',          icon: square    },
+    { name: 'Train yourself',   path: '/Training',      icon: square    },
+    { name: 'Explore & Equip',  path: '/ExploreEquip',  icon: triangle  },
+    { name: 'Navigate',         path: '/Navigate',      icon: images    },
+    { name: 'Assistance',       path: '/Assistance',    icon: language  }
+  ]
+}*/
+
+interface FooterMenu {
+  name: string,
+  path: string,
+  icon: {
+    url: string,
+  },
+  tag: string,
+  app_hooks_translation: {
+    title_translation: string,
+    language_translation: {
+      tag: string
+    }
+  }
+}
+
+interface DispatchProps {
+  loadConfData: typeof loadConfData;
+  loadUserData: typeof loadUserData;
+  setIsLoggedIn: typeof setIsLoggedIn;
+  setUsername: typeof setUsername;
+}
 
 const App: React.FC = () => {
 
   const [hooks, setHooks] = useState<any[]>([])
 
+  useIonViewDidEnter(() => {
+    console.log('APP ionViewDidEnter event fired');
+  });
+
+  useIonViewDidLeave(() => {
+    console.log('APP ionViewDidLeave event fired');
+  });
+
+  useIonViewWillEnter(() => {
+    console.log('APP ionViewWillEnter event fired');
+  });
+
+  useIonViewWillLeave(() => {
+    console.log('APP ionViewWillLeave event fired');
+  });
+
+  function renderFooterMenu(list: FooterMenu[]) {
+    /*var list2
+    list.forEach( function(valor1, indice, array) {
+        var ter = valor1.app_hooks_translation.map( function(valor, indice, array) {
+          return valor.title_translation
+        })
+        alert(ter)
+    })
+
+    //alert(JSON.stringify(list2))
+*/
+    var r = list
+      .filter(route => !!route.path)
+      .map(p => (
+        <IonTabButton tab={p.path+p.tag} href={p.path}>
+          <img src={Const.PHOTO_STORAGE + p.icon.url} alt="Ionic logo" />
+          <IonLabel>{p.name}</IonLabel>
+        </IonTabButton>
+      )
+    )
+    //renderConsole(hooks)
+    return r
+  }
+
+
+  function renderConsole(list: FooterMenu[]) {
+    console.log(list[0])
+  }
+  
   useEffect(() => {
     fetch( Const.RestAPI + 'hooks?style=main')
-      .then(res => res.json())
-      .then(setHooks)
+    .then(res => res.json())
+    .then(setHooks)
   }, [])
+  
+  //renderConsole(hooks)
 
   return(
     <IonApp>
       <IonReactRouter>
         <IonTabs>
-          
           <IonRouterOutlet>
-            <Route path='/Home'           component={Home}          />
-            <Route path='/Training'       component={Training}      />
-            <Route path='/ExploreEquip'   component={ExploreEquip}  />
-            <Route path='/Navigate'       component={Navigate}      />
-            <Route path='/Routes'         component={Routes}      />
-            <Route path='/Route1'         component={Route1}      />
-            <Route path='/LiveMap'        component={LiveMap}      />
-            <Route path='/BoatTypes'      component={BoatTypes}      />
-            <Route path='/Assistance'     component={Assistance}    />
-            <Route path='/' render={ () => <Redirect to='/Home'   /> } exact={true} />
+            <Route path='/Home' component={Home}/>
+            <Route path='/Training' component={Training}/>
+            <Route path='/ExploreEquip' component={ExploreEquip}/>
+            <Route path='/Navigate' component={Navigate}/>
+            <Route path='/Assistance' component={Assistance}/>
+            <Route path='/LiveMenu/:tag' component={LiveMenu}/>
+            <Route path='/' render={() => <Redirect to='/Home'/>} exact={true}/>
+            {/*<Route path="/logout" render={() => {
+              return <RedirectToLogin
+                setIsLoggedIn={setIsLoggedIn}
+                setUsername={setUsername}
+              />;
+            }} />*/}        
           </IonRouterOutlet>
-
           <IonTabBar slot='bottom'>
-            {hooks.map((hook, index) => {
-              return(            
-                <IonTabButton tab={hook.path} href={hook.path}>
-                  <IonIcon icon={triangle} />
-                  <IonLabel>Home</IonLabel>
-                </IonTabButton>
-              )
-            })}
+            <IonTabButton tab='/Home' href="/Home">
+            <IonIcon icon={triangle} />
+            <IonLabel>Home</IonLabel>
+          </IonTabButton>
+            {renderFooterMenu(hooks)}
           </IonTabBar>
-          {/*<IonTabBar slot='bottom'>
-            
-            <IonTabButton tab='Home' href='/Home'>
-              <IonIcon icon={triangle} />
-              <IonLabel>Home</IonLabel>
-            </IonTabButton>
-
-            <IonTabButton tab='Training' href='/training'>
-              <IonIcon icon={images} />
-              <IonLabel>Train yourself</IonLabel>
-            </IonTabButton>
-
-            <IonTabButton tab='ExploreEquip' href='/ExploreEquip'>
-              <IonIcon icon={square} />
-              <IonLabel>Explore & Equipament</IonLabel>
-            </IonTabButton>
-
-            <IonTabButton tab='Navigate' href='/Navigate'>
-              <IonIcon icon={square} />
-              <IonLabel>Navigate</IonLabel>
-            </IonTabButton>
-
-            <IonTabButton tab='Assistance' href='/Assistance'>
-              <IonIcon icon={square} />
-              <IonLabel>Assistance</IonLabel>
-            </IonTabButton>
-
-          </IonTabBar>*/}
-
         </IonTabs>
-
       </IonReactRouter>
-
     </IonApp>
-
   )
 
 }
--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './ExploreContainer.css';
+import '../../style/ExploreContainer.css';
 
 interface ContainerProps {
   name: string;
